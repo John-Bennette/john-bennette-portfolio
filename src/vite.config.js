f70526d975cdd5e46e3c391678d173d611fe6178
illusionofchoice <jbennette23@gmail.com>
2024-11-06 09:13:50 -0800

    Simplified vite config and rebuilt

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,23 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/john-bennette-portfolio/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (/\.(gif|jpe?g|png|svg)$/i.test(assetInfo.name)) {
-            return 'images/[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
     }
   }
 })
